9e0fdd65ca2cf4e2ec5209a1b33634cbdb24ec41
Emily Flion <emneo@kreog.com>
2025-08-12 20:05:30 +0200

    Add first release of libgomoku - 0.1.0
    Basically just the same ugly code, but at least it works.
    
    Signed-off-by: Emily Flion <emneo@kreog.com>

---
+++ build.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const llvm = b.option(bool, "llvm", "Use LLVM backend") orelse true;
+
+    _ = b.addModule("gomoku_protocol", .{
+        .root_source_file = b.path("src/protocol.zig"),
+    });
+    _ = b.addModule("gomoku_game", .{
+        .root_source_file = b.path("src/game.zig"),
+    });
+
+    const test_step_protocol = b.step("test_protocol", "Run protocol unit tests");
+
+    const unit_tests_protocol = b.addTest(.{
+        .root_source_file = b.path("src/protocol.zig"),
+        .target = target,
+        .optimize = optimize,
+        .test_runner = .{ .path = b.path("test_runner.zig"), .mode = .simple },
+        .use_llvm = llvm,
+    });
+
+    const run_unit_tests_protocol = b.addRunArtifact(unit_tests_protocol);
+    run_unit_tests_protocol.has_side_effects = true;
+    test_step_protocol.dependOn(&run_unit_tests_protocol.step);
+
+    const test_step_game = b.step("test_game", "Run game unit tests");
+
+    const unit_tests_game = b.addTest(.{
+        .root_source_file = b.path("src/game.zig"),
+        .target = target,
+        .optimize = optimize,
+        .test_runner = .{ .path = b.path("test_runner.zig"), .mode = .simple },
+        .use_llvm = llvm,
+    });
+
+    const run_unit_tests_game = b.addRunArtifact(unit_tests_game);
+    run_unit_tests_game.has_side_effects = true;
+    test_step_game.dependOn(&run_unit_tests_game.step);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests_game.step);
+    test_step.dependOn(&run_unit_tests_protocol.step);
+}
